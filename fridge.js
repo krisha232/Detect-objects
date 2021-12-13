4e4cf76f8f37f9caed8731c7b2b143dee501914b
krisha232 <78255450+krisha232@users.noreply.github.com>
2021-12-13 16:53:30 +0530

    Update fridge.js

--- fridge.js
+++ fridge.js
@@ -14,6 +14,22 @@ function preload() {
 }
 function draw() {
     image(img, 0, 0, 640, 420);
+    if(status1 !=""){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+             for(i=0; i<objects.length; i++){
+
+            document.getElementById("status").innerHTML="Status : Object Detected ";
+            document.getElementById("identify").innerHTML="Number of objects detected are :" + objects.length;
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+"" + percent + "%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
 
 function modelLoaded() {
